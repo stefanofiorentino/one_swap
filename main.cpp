8f4d983e80e1c088e5116d40d8e263dcc6f60b8a
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-01-25 14:09:05 +0100

    solutions: 	- naive 	- n^2 	- nlogn (??)
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,9 +2,79 @@
 #include <fstream>
 #include <vector>
 #include <iosfwd>
+#include <algorithm>
 
+inline int naive_solution(std::vector<int> &int_vec)
+{
+    auto sorted_int_vec{int_vec};
+    std::sort(std::begin(sorted_int_vec), std::end(sorted_int_vec));
+    int changes = 0;
+    for (auto it = 0u; it < int_vec.size(); ++it)
+    {
+        if (sorted_int_vec.at(it) != int_vec.at(it))
+        {
+            ++changes;
+        }
+    }
+    if (0 == changes)
+    {
+        return 0;
+    }
+    else if (2 == changes)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
 
-void solution(std::istream &in, std::ostream &out)
+inline int n2_solution(std::vector<int> &int_vec)
+{
+    if (std::is_sorted(std::begin(int_vec), std::end(int_vec)))
+    {
+        return 0;
+    }
+    for (auto i = 0u; i < int_vec.size(); ++i)
+    {
+        for (auto j = i + 1; j < int_vec.size(); ++j)
+        {
+            std::swap(int_vec.at(i), int_vec.at(j));
+            if (std::is_sorted(std::begin(int_vec), std::end(int_vec)))
+            {
+                std::swap(int_vec.at(j), int_vec.at(i));
+                return 1;
+            }
+            std::swap(int_vec.at(j), int_vec.at(i));
+        }
+    }
+    return -1;
+}
+
+inline int nlogn_solution(std::vector<int> &int_vec)
+{
+    if (std::is_sorted(std::begin(int_vec), std::end(int_vec)))
+    {
+        return 0;
+    }
+    for (auto i = 0u; i < int_vec.size(); ++i)
+    {
+        for (auto j = int_vec.size() - 1; j > i; --j)
+        {
+            std::swap(int_vec.at(i), int_vec.at(j));
+            if (std::is_sorted(std::begin(int_vec), std::end(int_vec)))
+            {
+                std::swap(int_vec.at(j), int_vec.at(i));
+                return 1;
+            }
+            std::swap(int_vec.at(j), int_vec.at(i));
+        }
+    }
+    return -1;
+}
+
+inline void solution(std::istream &in, std::ostream &out)
 {
     int numel = -1;
     std::cin >> numel;
@@ -15,15 +85,23 @@ void solution(std::istream &in, std::ostream &out)
         std::cin >> int_temp;
         int_vec.emplace_back(int_temp);
     }
-    return;
+//    out << naive_solution(int_vec) << "\n";
+//    out << n2_solution(int_vec) << "\n";
+    out << nlogn_solution(int_vec) << "\n";
 }
 
 
 int main()
 {
-    std::ifstream in("../input.txt");
-    std::cin.rdbuf(in.rdbuf());
+//    std::ifstream in("../input.txt");
+//    std::cin.rdbuf(in.rdbuf());
+//
+//    solution(std::cin, std::cout);
+
+    std::ifstream in1("../input.at.scale.txt");
+    std::cin.rdbuf(in1.rdbuf());
 
     solution(std::cin, std::cout);
+
     return 0;
 }
